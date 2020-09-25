5796cf7b0d84c37ba587f774519bb6fa1816500e
mark <markpackham1@gmail.com>
2020-09-25 12:19:41 +0100

    finish off system tests, https://youtu.be/B3Fbujmgo60?t=6682

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
     test "is valid with valid attributes" do
-        entry = Entry.new(meal_type: "Breakfast", carbohydrates:100, proteins:200, calories: 300, fats: 400)
+        entry = Entry.new(meal_type: "Breakfast", carbohydrates: 100, proteins: 200, calories: 300, fats: 400)
         assert entry.save
     end
 end
